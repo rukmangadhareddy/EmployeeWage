68a6324e7ae4037f7510cdd3282268947587831e
rukmangadhareddy <rukmangadhareddy43@gmail.com>
2023-12-29 23:13:21 +0530

    UC2_EmployeeWage

--- src/com/bridgelabzs/practice/EmployeeWage.java
+++ src/com/bridgelabzs/practice/EmployeeWage.java
@@ -1,27 +1,32 @@
 package com.bridgelabzs.practice;
 
 
-	public class EmployeeWage {
 
+public class EmployeeWage {
 
-			
-			static final int PRESENT = 1;
-		    static final int ABSENT = 0;
-		    static final int Employee_Wage = 0;
-		    public static void employee(){
-		        int randomNumber = (int) Math.floor(Math.random()*10)%2;
-		        if (randomNumber ==1){
-		        	
-		            System.out.println("Employee is Present");
-		        }
-		        else {
-		            System.out.println("Employee is Absent");
-		        }
-		    }
+    static final int FULL_DAY = 1;
+    static final int FULL_DAY_HOUR=8;
+    static final int WAGE_PER_HOUR=20;
+    static final int ABSENT = 0;
 
-		    public static void main(String[] args) {
-		        employee();
-		    }
-		}
+    public static void employee(){
+        int empWage = 0;
 
+        int randomNumber = (int) Math.floor(Math.random()*10)%2;
+        if (randomNumber ==FULL_DAY){
+            System.out.println("Employee is Present");
+            empWage = FULL_DAY_HOUR*WAGE_PER_HOUR;
+
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Employee wage is : "+empWage);
+    }
+
+    public static void main(String[] args) {
+        employee();
+    }
+
+}
 
