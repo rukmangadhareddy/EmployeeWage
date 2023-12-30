18a85f9b45d2fae7f569a77eeee76aab7e684402
rukmangadhareddy <rukmangadhareddy43@gmail.com>
2023-12-30 10:18:49 +0530

    UC5_CalculateEmployeeWagePerMonth

--- src/com/bridgelabzs/practice/EmployeeWage.java
+++ src/com/bridgelabzs/practice/EmployeeWage.java
@@ -1,27 +1,55 @@
 package com.bridgelabzs.practice;
 
 
-
 public class EmployeeWage {
 
     static final int FULL_DAY = 1;
+    static final int HALF_DAY = 2;
     static final int FULL_DAY_HOUR=8;
+    static final int HALF_DAY_HOUR=4;
     static final int WAGE_PER_HOUR=20;
+
     static final int ABSENT = 0;
 
-    public static void employee(){
+    public static void employee() {
         int empWage = 0;
+        int hour = 0;
+        int totalEmpWage = 0;
+        int totalHours = 0;
+        int monthWorkingDays=20;
+        int days = 1;
+        while (days<=monthWorkingDays){
 
-        int randomNumber = (int) Math.floor(Math.random()*10)%2;
-        if (randomNumber ==FULL_DAY){
-            System.out.println("Employee is Present");
-            empWage = FULL_DAY_HOUR*WAGE_PER_HOUR;
+        int randomNumber = (int) Math.floor(Math.random() * 10) % 3;
+        switch (randomNumber) {
+            case (FULL_DAY):
+
+                empWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                hour=FULL_DAY_HOUR;
+                System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hour is "+hour);
+                break;
+
+            case (HALF_DAY):
+
+                empWage = HALF_DAY_HOUR * WAGE_PER_HOUR;
+                hour=HALF_DAY_HOUR;
+                System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hours is "+hour);
+                break;
+            default:
+                hour=0;
+                empWage=0;
+                System.out.println("Day - "+days+" : Employee is Absent");
+
 
         }
-        else {
-            System.out.println("Employee is Absent");
+
+        totalEmpWage=totalEmpWage+empWage;
+        totalHours=totalHours+hour;
+        days++;
+
         }
-        System.out.println("Employee wage is : "+empWage);
+        System.out.println("Total Employee wage : "+totalEmpWage);
+        System.out.println("Total Employee working hours : "+totalHours);
     }
 
     public static void main(String[] args) {
