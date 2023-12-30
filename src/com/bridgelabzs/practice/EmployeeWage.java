138c38e1f2369c3cc241894d90db9c6a83fd318f
rukmangadhareddy <rukmangadhareddy43@gmail.com>
2023-12-30 10:44:26 +0530

    UC7_UseClassMethodToComputeEmployeeWage

--- src/com/bridgelabzs/practice/EmployeeWage.java
+++ src/com/bridgelabzs/practice/EmployeeWage.java
@@ -1,36 +1,41 @@
 package com.bridgelabzs.practice;
 
 
+
 public class EmployeeWage {
-
-    static final int FULL_DAY = 1;
-    static final int HALF_DAY = 2;
-    static final int FULL_DAY_HOUR=8;
-    static final int HALF_DAY_HOUR=4;
-    static final int WAGE_PER_HOUR=20;
-
-    static final int ABSENT = 0;
-
+    /**
+     * compute the employee wage and working hours use random method
+     * use class level variables
+     */
+    public static final int ABSENT = 0;
+    public static final int FULL_DAY = 1;
+    public static final int HALF_DAY = 2;
+    public static final int FULL_DAY_HOUR=8;
+    public static final int HALF_DAY_HOUR=4;
+    public static final int WAGE_PER_HOUR=20;
+    public static final int MONTHLY_WORKING_HOURS =92;
+    public static final int MONTHLY_WORKING_DAYS =20;
+    static int empWage;
+    static int hour;
+    static int totalEmpWage = 0;
+    static int totalHours = 0;
     public static void employee() {
-        int empWage = 0;
-        int hour = 0;
-        int totalEmpWage = 0;
-        int totalHours = 0;
-        int monthWorkingDays=20;
+        /**
+         * find the total wage and hours in month.
+         */
         int days = 1;
-        while (days<=monthWorkingDays && totalHours<100){
-
-        int randomNumber = (int) Math.floor(Math.random() * 10) % 3;
+        while (days<=MONTHLY_WORKING_DAYS && totalHours<MONTHLY_WORKING_HOURS){
+            /**
+             * use the Math class and random(static method).
+             */
+        int randomNumber = (int) (Math.random() * 10) % 3;
         switch (randomNumber) {
             case (FULL_DAY):
-
                 empWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
                 hour=FULL_DAY_HOUR;
                 System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hour is "+hour);
                 break;
-
             case (HALF_DAY):
-
                 empWage = HALF_DAY_HOUR * WAGE_PER_HOUR;
                 hour=HALF_DAY_HOUR;
                 System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hours is "+hour);
@@ -39,20 +44,18 @@ public class EmployeeWage {
                 hour=0;
                 empWage=0;
                 System.out.println("Day - "+days+" : Employee is Absent");
-
-
         }
-
         totalEmpWage=totalEmpWage+empWage;
         totalHours=totalHours+hour;
         days++;
-
         }
         System.out.println("Total Employee wage : "+totalEmpWage);
         System.out.println("Total Employee working hours : "+totalHours);
     }
-
     public static void main(String[] args) {
+        /**
+         * method calling directly
+         */
         employee();
     }
 
