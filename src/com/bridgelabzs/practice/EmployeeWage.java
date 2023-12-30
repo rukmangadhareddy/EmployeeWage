675f6172cf1458e6b84ff6671af6ff46a4cbc2b7
rukmangadhareddy <rukmangadhareddy43@gmail.com>
2023-12-30 08:39:23 +0530

    Uc3_EMployeeHalfDayWage

--- src/com/bridgelabzs/practice/EmployeeWage.java
+++ src/com/bridgelabzs/practice/EmployeeWage.java
@@ -5,20 +5,26 @@ package com.bridgelabzs.practice;
 public class EmployeeWage {
 
     static final int FULL_DAY = 1;
+    static final int HALF_DAY = 2;
     static final int FULL_DAY_HOUR=8;
+    static final int HALF_DAY_HOUR=4;
     static final int WAGE_PER_HOUR=20;
+
     static final int ABSENT = 0;
 
     public static void employee(){
         int empWage = 0;
 
-        int randomNumber = (int) Math.floor(Math.random()*10)%2;
+        int randomNumber = (int) Math.floor(Math.random()*10)%3;
         if (randomNumber ==FULL_DAY){
-            System.out.println("Employee is Present");
+            System.out.println("Employee is full day Present");
             empWage = FULL_DAY_HOUR*WAGE_PER_HOUR;
 
-        }
-        else {
+        } else if (randomNumber == HALF_DAY) {
+            System.out.println("Employee is half day Present");
+            empWage = HALF_DAY_HOUR*WAGE_PER_HOUR;
+
+        } else {
             System.out.println("Employee is Absent");
         }
         System.out.println("Employee wage is : "+empWage);
