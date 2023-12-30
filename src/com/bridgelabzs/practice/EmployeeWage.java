5051c0932bfed6ceb953521d772999781bf1a9df
rukmangadhareddy <rukmangadhareddy43@gmail.com>
2023-12-30 10:54:48 +0530

    UC8_AddMultipleCompanies

--- src/com/bridgelabzs/practice/EmployeeWage.java
+++ src/com/bridgelabzs/practice/EmployeeWage.java
@@ -2,41 +2,42 @@ package com.bridgelabzs.practice;
 
 
 
+import java.util.Scanner;
+/**
+ * @author GovardhanReddy
+ */
 public class EmployeeWage {
     /**
      * compute the employee wage and working hours use random method
-     * use class level variables
+     * use class method functions
      */
-    public static final int ABSENT = 0;
     public static final int FULL_DAY = 1;
     public static final int HALF_DAY = 2;
     public static final int FULL_DAY_HOUR=8;
     public static final int HALF_DAY_HOUR=4;
-    public static final int WAGE_PER_HOUR=20;
-    public static final int MONTHLY_WORKING_HOURS =92;
-    public static final int MONTHLY_WORKING_DAYS =20;
-    static int empWage;
-    static int hour;
-    static int totalEmpWage = 0;
-    static int totalHours = 0;
-    public static void employee() {
+    public static void employee(String companyName ,int wagePerHour ,int monthlyWorkingDays ,int monthlyWorkingHours ) {
         /**
          * find the total wage and hours in month.
          */
+        System.out.println("Company name is "+companyName +" : ");
+        int empWage;
+        int hour;
+        int totalEmpWage = 0;
+        int totalHours = 0;
         int days = 1;
-        while (days<=MONTHLY_WORKING_DAYS && totalHours<MONTHLY_WORKING_HOURS){
+        while (days<=monthlyWorkingDays && totalHours<monthlyWorkingHours){
             /**
              * use the Math class and random(static method).
              */
         int randomNumber = (int) (Math.random() * 10) % 3;
         switch (randomNumber) {
             case (FULL_DAY):
-                empWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                empWage = FULL_DAY_HOUR * wagePerHour;
                 hour=FULL_DAY_HOUR;
                 System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hour is "+hour);
                 break;
             case (HALF_DAY):
-                empWage = HALF_DAY_HOUR * WAGE_PER_HOUR;
+                empWage = HALF_DAY_HOUR * wagePerHour;
                 hour=HALF_DAY_HOUR;
                 System.out.println("Day - "+days+" : Employee wage is "+empWage+" and hours is "+hour);
                 break;
@@ -49,16 +50,29 @@ public class EmployeeWage {
         totalHours=totalHours+hour;
         days++;
         }
-        System.out.println("Total Employee wage : "+totalEmpWage);
-        System.out.println("Total Employee working hours : "+totalHours);
+        System.out.println(companyName+" company total Employee wage : "+totalEmpWage);
+        System.out.println(companyName+" company total Employee working hours : "+totalHours);
     }
     public static void main(String[] args) {
         /**
-         * method calling directly
+         * static method calling directly
          */
-        employee();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter how many company details : ");
+        int numberOfCompanies = input.nextInt();
+        for (int i=1;i<=numberOfCompanies;i++){
+            System.out.print("Enter "+i+" company name : ");
+            String name = input.next();
+            System.out.print("Enter wage per hour : ");
+            int wagePerHour = input.nextInt();
+            System.out.print("Enter monthly working days : ");
+            int monthlyWorkingDays = input.nextInt();
+            System.out.print("Enter monthly working hour : ");
+            int monthlyWorkingHours = input.nextInt();
+            employee(name,wagePerHour,monthlyWorkingDays,monthlyWorkingHours);
+        }
     }
-
 }
+
 
 
